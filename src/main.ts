74a501240db49629c3b40990f046baf91d325196
Tianjian <mail@isdtj.com>
2022-11-19 17:54:22 +0800

    u

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { App, createApp } from 'vue'
 import RootEl from './App.vue'
 import Router from './router';
-import hljsVuePlugin from './foundation/app/highlight';
 import "./assets/common.css";
 import 'vant/lib/index.css';
 
 const Ins: App<Element> = createApp(RootEl);
 Ins.use(Router);
-Ins.use(hljsVuePlugin);
 Ins.mount('#app');
